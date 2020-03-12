a2eeca309f4a3bee80256acfad14dc7f884b4d56
henning <henning.nobbe@innoq.com>
2020-03-12 01:46:24 +0100

    include initial testOverviewJsonFile and save result in testOverview.json

--- src/wertgarantieTestCrawler.js
+++ src/wertgarantieTestCrawler.js
@@ -1,101 +1,107 @@
 const axios = require('axios');
 const _ = require('lodash');
+const fs = require('fs');
+const currentTestOverview = require('./testOverview');
+const username = 'wertgarantie-ecom';
+const vcsType = 'github';
 
-const config = {
-    username: 'wertgarantie-ecom',
-    projects: [
-        {
-            projectName: 'bifrost',
-            testJobName: 'build'
-        },
-        {
-            projectName: 'bifrost-components',
-            testJobName: 'test'
-        }],
-    vcsType: 'github',
-    circleCiToken: process.env.CIRCLE_CI_TOKEN
-};
+const circleCiAPI = `https://circleci.com/api/v1.1/project/${vcsType}/${username}`;
 
-const result = {};
-const circleCiAPI = `https://circleci.com/api/v1.1/project/${config.vcsType}/${config.username}`;
+for (var i = 0; i < currentTestOverview.length; i++) {
+    if (!currentTestOverview[i].lastIncludedBuildNumber) {
+        currentTestOverview[i].lastIncludedBuildNumber = 0;
+    }
+}
 
-const latestBuildNumbers = getLatestBuildNumbers()
-    .then(projects => getBuildDataForProjects(projects))
-    .then(projects => addTestMetadataToProjects(projects))
-    .then(projects => console.log(JSON.stringify(projects, null, 2)));
+getLatestBuildNumbers()
+    .then(getBuildDataForProjects)
+    .then(addTestMetadataToProjects)
+    .then(projects => {
+        console.log(JSON.stringify(projects, null, 2));
+        return projects;
+    })
+    .then(saveToTestOverviewFile);
 
 function getLatestBuildNumbers() {
-    return Promise.all(config.projects.map(async (project) => {
+    return Promise.all(currentTestOverview.map(async (project) => {
         const buildNumbersUrl = `${circleCiAPI}/${project.projectName}/tree/master`;
         const response = await axios.get(buildNumbersUrl);
         if (response && response.data) {
             const latestTestJob = _.find(response.data, (buildStep) => buildStep.build_parameters.CIRCLE_JOB === project.testJobName);
-            project.maxBuildNumber = latestTestJob.build_num;
+            project.maxIncludedBuildNumber = latestTestJob.build_num;
             return project;
         }
         return undefined;
     }));
 }
 
-function getBuildDataForProjects(projects) {
-    console.log(`get all builds for projects ${JSON.stringify(projects, null, 2)}`);
-    return Promise.all(projects.map(async project => {
-            const testBuilds = [];
-            for (let buildNumber = project.maxBuildNumber; buildNumber <= project.maxBuildNumber; buildNumber++) {
-                const buildNumbersUrl = `${circleCiAPI}/${project.projectName}/${buildNumber}`;
-                console.log(`requesting build data for project ${project.projectName} and build ${buildNumber}`);
-                const response = await axios.get(buildNumbersUrl);
-                if (response && response.data && response.data.build_parameters.CIRCLE_JOB === project.testJobName && response.data.branch === 'master') {
-                    testBuilds.push({
-                        status: response.data.status,
-                        startTime: response.data.start_time,
-                        buildNumber: response.data.build_num,
-                        commitDetails: response.data.all_commit_details
-                    });
-                }
-                console.log(`received build data for project ${project.projectName} and build ${buildNumber}`);
+async function getBuildDataForProjects(projects) {
+    return await Promise.all(projects.map(async project => {
+        for (let buildNumber = project.lastIncludedBuildNumber + 1; buildNumber <= project.maxIncludedBuildNumber; buildNumber++) {
+            const buildNumbersUrl = `${circleCiAPI}/${project.projectName}/${buildNumber}`;
+            console.log(`requesting build data for project ${project.projectName} and build ${buildNumber}`);
+            const response = await axios.get(buildNumbersUrl);
+            if (response && response.data && response.data.build_parameters.CIRCLE_JOB === project.testJobName && response.data.branch === 'master') {
+                project.testBuilds.push({
+                    status: response.data.status,
+                    startTime: response.data.start_time,
+                    buildNumber: response.data.build_num,
+                    commitDetails: response.data.all_commit_details
+                });
             }
-            project.testBuilds = testBuilds;
-            return project;
+            console.log(`received build data for project ${project.projectName} and build ${buildNumber}`);
         }
-    ));
+        project.lastIncludedBuildNumber = project.maxIncludedBuildNumber;
+        return project;
+    }));
 }
 
 async function addTestMetadataToProjects(projects) {
-    await Promise.all(projects.map(async project => {
-        addTestMetadataToProject(project);
+    return await Promise.all(projects.map(async project => {
+        const testBuilds = await Promise.all(project.testBuilds.map(async testBuild => {
+            const metaData = await getTestMetadataToTestBuild(project.projectName, testBuild);
+            if (metaData.count === 0) {
+                return;
+            }
+            testBuild.tests = metaData;
+            const reports = await getReportsForTestBuild(project.projectName, project.reports, testBuild);
+            testBuild.reports = reports;
+            return testBuild;
+        }));
+        project.testBuilds = testBuilds;
+        return project;
     }));
-    return projects;
 }
 
-
-async function addTestMetadataToProject(project) {
-    await Promise.all(project.testBuilds.map(async testBuild => {
-        await addTestMetadataToTestBuild(project.projectName, testBuild);
-        return testBuild;
-    }));
-    return project;
-}
-
-async function addTestMetadataToTestBuild(projectName, testBuild) {
-    const buildNumbersUrl = `${circleCiAPI}/${projectName}/${testBuild.buildNumber}/tests`;
-    const response = await axios.get(buildNumbersUrl);
+async function getTestMetadataToTestBuild(projectName, testBuild) {
+    const testsUrl = `${circleCiAPI}/${projectName}/${testBuild.buildNumber}/tests`;
+    const response = await axios.get(testsUrl);
     if (response && response.data) {
         const allTests = response.data.tests.length;
         const results = _.countBy(response.data.tests, test => test.result);
-        testBuild.tests = (tests = {
+        return {
             results: results,
             count: allTests
-        });
-        return testBuild;
+        };
     }
 }
 
-function getArtifacts() {
-
+async function getReportsForTestBuild(projectName, reports, testBuild) {
+    const artifactsUrl = `${circleCiAPI}/${projectName}/${testBuild.buildNumber}/artifacts`;
+    const response = await axios.get(artifactsUrl);
+    if (response && response.data) {
+        return reports.map(report => {
+            const artifact = _.find(response.data, (artifact) => artifact.path === report.path);
+            if (artifact) {
+                return {
+                    reportName: report.name,
+                    artifactUrl: artifact.url
+                };
+            }
+        });
+    }
 }
 
-//
-// curl https://circleci.com/api/v1.1/project/:vcs-type/:username/:project/latest/artifacts?circle-token=:token&branch=:branch&filter=:filter
-//
-// curl https://circleci.com/api/v1.1/project/github/wertgarantie-ecom/bifrost?circle-token=${TOKEN}&limit=1&offset=5&filter=completed
+function saveToTestOverviewFile(projects) {
+    fs.writeFileSync('src/testOverview.json', JSON.stringify(projects, null, 2));
+}
